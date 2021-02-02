f94fc3d7be7c7d46e248b9e73d45d893eaa0a3c7
Shreyansh Mehta <shreyansh887121@gmail.com>
2021-02-03 02:10:44 +0530

    Modal Added

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,9 +1,15 @@
 var join_btn = document.getElementById("join-btn");
 var create_btn = document.getElementById("create-btn");
+var modal = document.getElementById("bg-modal");
+var close = document.querySelector(".close");
 
 join_btn.onclick = function() {
-    console.log("clicked");
+    modal.style.display = "flex";
 }
+
+close.addEventListener("click", function() {
+    modal.style.display = "none";
+});
 
 create_btn.onclick = function() {
     console.log("created clicked");
