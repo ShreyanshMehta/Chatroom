4801852e0c053b9dc8e296848939c8a563417c1b
Shreyansh Mehta <shreyansh887121@gmail.com>
2021-02-02 23:41:51 +0530

    Home created

---
+++ static/scripts/home.js
@@ -0,0 +1,10 @@
+var join_btn = document.getElementById("join-btn");
+var create_btn = document.getElementById("create-btn");
+
+join_btn.onclick = function() {
+    console.log("clicked");
+}
+
+create_btn.onclick = function() {
+    console.log("created clicked");
+}
